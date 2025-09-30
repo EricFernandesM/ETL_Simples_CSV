f1cf9c8bcf34bf18c610f668783f2c3f94cc3c6a
Eric Fernandes Martins <eric.fmartins11@gmail.com>
2025-09-29 21:45:32 -0300

    Adição da função de carregamento de arquivo

--- etl.py
+++ etl.py
@@ -15,3 +15,12 @@ def calcular_kpi_de_vendas_total(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 # uma função que carrega o arquivo
+
+def carregar_dados(df: pd.DataFrame, format_saida: list):
+    for formato in format_saida:
+        if format == 'csv':
+            df.to_csv("dados.csv")
+        if format == 'parquet':
+            df.to_parquet("dados_parquet")
+
+
